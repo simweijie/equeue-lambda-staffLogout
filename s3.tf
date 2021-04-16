5d538ada4e59e8be5fdcf22cf4b71b97b430c8fb
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-16 21:09:37 +0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "nus-iss-equeue-terraform"
-    key    = "lambda/listOfClinics/tfstate"
+    key    = "lambda/staffLogout/tfstate"
     region = "us-east-1"
   }
 }
